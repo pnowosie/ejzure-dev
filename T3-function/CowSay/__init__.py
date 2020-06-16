2f9b49a106e11e73502e4ead28cb1e1b761412dd
Pawel Nowosielski <Pawel.Nowosielski@imapp.pl>
2020-06-16 14:18:28 +0200

    Add timeout to open db connection to deal with coldstarts

--- T3-function/CowSay/__init__.py
+++ T3-function/CowSay/__init__.py
@@ -5,20 +5,25 @@ import pyodbc
 import azure.functions as func
 
 
+QUERY = 'SELECT TOP 1 author, quote FROM quotes ORDER BY newid()'
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     # connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=<server>.database.windows.net;PORT=1433;DATABASE=<database>;UID=<user>;PWD=<password>'
     connection_string = os.environ['CONNECTION_STRING']
-    
-    conn = pyodbc.connect(connection_string)
 
-    cur = conn.cursor()
-    cur.execute('SELECT TOP 1 author, quote FROM quotes ORDER BY newid();')
-    author, quote = cur.fetchone()
+    try:
+        conn = pyodbc.connect(connection_string, timeout=3)
+
+        cur = conn.cursor()
+        author, quote = cur.execute(QUERY).fetchone()
+
+        cur.close()
+        conn.close()
+    except pyodbc.OperationalError:
+        author, quote = '503 Service Unavailable', 'Včera jsme měli párty a pili jsme těžko...'
 
     resp = {"author": author, "quote": quote}
-    cur.close()
-    conn.close()
 
     return func.HttpResponse(
         json.dumps(resp),
